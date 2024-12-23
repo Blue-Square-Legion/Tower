a248376bf512c54c2514a22a0a56c796660caa16
AceSpadeR <aspader.12@gmail.com>
2024-12-22 17:22:13 -0700

    fixed bug

--- Assets/SpikeTrigger.cs
+++ Assets/SpikeTrigger.cs
@@ -26,6 +26,7 @@ public class SpikeTrigger : MonoBehaviour
 
         if (other.transform.CompareTag("Enemy"))
         {
+            Debug.Log("enter");
             enemiesInside.Add(other.gameObject.GetComponent<Enemy>());
         }
     }
@@ -35,15 +36,18 @@ public class SpikeTrigger : MonoBehaviour
 
         if (other.transform.CompareTag("Enemy"))
         {
+            Debug.Log("exit");
             enemiesInside.Remove(other.gameObject.GetComponent<Enemy>());
         }
     }
 
     public void damageTick()
     {
+        enemiesInside.RemoveAll(enemy => enemy == null || !enemy.gameObject.activeInHierarchy);
         int enemyCount = enemiesInside.Count;
         for (int i = 0; i < enemyCount; i++)
         {
+            Debug.Log(enemiesInside[i]);
             enemiesInside[i].lastDamagingTower = transform.GetComponent<TowerBehavior>();
             gameManager.EnqueueDamageData(new GameManager.EnemyDamageData(enemiesInside[i], damage, 
                 enemiesInside[i].damageResistance, transform.GetComponent<TowerBehavior>()));
