c4273aeed7f402597c68ae2ac34617b1ba4aca86
LavanGrimwulff <31390396+LavanGrimwulff@users.noreply.github.com>
2025-01-29 11:29:12 -0600

    Visuals for cards in hand
    Currently not working with the camera properly but otherwise mostly functional

--- Assets/Scripts/Managers/Card.cs
+++ Assets/Scripts/Managers/Card.cs
@@ -1,11 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Card : MonoBehaviour
 {
+    //Need an image/3d model variable
+    [SerializeField] private TMP_Text cost;
+    [SerializeField] private TMP_Text cardName;
+    [SerializeField] private TMP_Text cardType;
+    [SerializeField] private TMP_Text effect;
+
+    public Card(int costIn, string cardNameIn, string cardTypeIn, string effectIn)
+    {
+        cost.text = "" + costIn;
+        cardName.text = cardNameIn;
+        cardType.text = cardTypeIn;
+        effect.text = effectIn;
+    }
+    public void setData(int costIn, string cardNameIn, string cardTypeIn, string effectIn)
+    {
+        cost.text = "" + costIn;
+        cardName.text = cardNameIn;
+        cardType.text = cardTypeIn;
+        effect.text = effectIn;
+    }
+
+
     private void clicked()
     {
 
     }    
+
+
+    private void execute()
+    {
+        //this is where we get to add the fun stuff later.
+    }
 }
--- Assets/Scripts/Managers/CardManager.cs
+++ Assets/Scripts/Managers/CardManager.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 
 public class CardManager : MonoBehaviour
 {
-    public List<Card> deck = new List<Card>();
-    public List<Card> hand = new List<Card>();
-    public List<Card> graveyard = new List<Card>();
+    public GameObject cardPrefab;
+    public Transform handPosition;
+
+    public List<GameObject> deck = new List<GameObject>();
+    public List<GameObject> hand = new List<GameObject>();
+    public List<GameObject> discard = new List<GameObject>();
 
 
+    float cardSpacing = 50f;
+    float vSpacing = 10f;
+
+
+    private void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject tempCard = Instantiate(cardPrefab, handPosition.position,Quaternion.identity);
+            tempCard.SetActive(false);
+            deck.Add(tempCard);
+        }
+    }
 
     public void drawCard()
     {
@@ -16,10 +33,12 @@ public class CardManager : MonoBehaviour
         {
             hand.Add(deck[0]);
             deck.RemoveAt(0);
+            hand[hand.Count-1].SetActive(true);
         }
         else
         {
-            addGraveyardIntoDeck();
+            Debug.Log("No cards in deck");
+            addDiscardIntoDeck();
 
             if (deck.Count > 0)
             {
@@ -27,14 +46,15 @@ public class CardManager : MonoBehaviour
                 deck.RemoveAt(0);
             }
         }
+        updateHandVisuals();
     }
 
-    public void addGraveyardIntoDeck()
+    public void addDiscardIntoDeck()
     {
-        while (graveyard.Count > 0)
+        while (discard.Count > 0)
         {
-            deck.Add(graveyard[0]);
-            graveyard.RemoveAt(0);
+            deck.Add(discard[0]);
+            discard.RemoveAt(0);
         }
         shuffle();
     }
@@ -46,7 +66,7 @@ public class CardManager : MonoBehaviour
         {
             n--;
             int k = Random.Range(0,n + 1);
-            Card temp = deck[k];
+            GameObject temp = deck[k];
             deck[k] = deck[n];
             deck[n] = temp;
         }
@@ -56,20 +76,52 @@ public class CardManager : MonoBehaviour
     {
         if (hand.Count > a)
         {
-            addToGraveyard(getCardInHandID(a));
+            addToDiscard(getCardInHandID(a));
             hand.RemoveAt(a);
         }
     }
 
-    public Card getCardInHandID(int pos)
+    public GameObject getCardInHandID(int pos)
     {
-        Card cardID = hand[pos];
+        GameObject cardID = hand[pos];
         return cardID;
     }
 
-    public void addToGraveyard(Card card)
+    public void addToDiscard(GameObject card)
     {
-        graveyard.Add(card);
+        discard.Add(card);
     }
 
+
+    public void updateHandVisuals()
+    {
+        int cards = hand.Count;
+
+        if (cards == 0)
+        {
+            //float hOffset = handPosition.position.x + cardSpacing * (i - (cards - 1) / 2f);
+           // hand[0].transform.localPosition = new Vector3(hOffset, 0f, 0f);
+        }
+        else
+        {
+            for (int i = 0; i < cards; i++)
+            {
+                float rotationAngle = -5f * (i - (cards - 1) / 2f);
+                hand[i].transform.localRotation = Quaternion.Euler(0f, 0f, rotationAngle);
+
+                float hOffset = handPosition.position.x + cardSpacing * (i - (cards - 1) / 2f);
+
+                float normalizedPos = 2 * (2f * i / (cards - 1) - 1f); //adjust for the arc
+                float vOffset = handPosition.position.y + vSpacing * (1 - normalizedPos * normalizedPos);
+
+                //set Position
+                hand[i].transform.localPosition = new Vector3(hOffset, vOffset, 0f);
+
+
+                //float handWidth = (hand.Count - 1 - i * 2) * cardWidth * gap;
+                //float position = handCenterPos - (handWidth / 2);
+                //hand[i].transform
+            }
+        }
+    }
 }
